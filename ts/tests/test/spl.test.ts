f8722e6766e90ab6a5ccf8817c04fd80d5a64860
billythedummy <dhy1996@live.com.sg>
2025-06-13 18:08:07 +0800

    add picosol prefund withdrawstake test

--- ts/tests/test/spl.test.ts
+++ ts/tests/test/spl.test.ts
@@ -3,6 +3,7 @@ import {
   depositSolFixturesTest,
   depositStakeFixturesTest,
   PICOSOL_MINT,
+  prefundWithdrawStakeFixturesTest,
   withdrawSolFixturesTest,
 } from "../utils";
 
@@ -10,13 +11,13 @@ describe("SPL Test", async () => {
   it("spl-stake-pool-deposit-sol", async () => {
     await depositSolFixturesTest(1000000n, PICOSOL_MINT, {
       inp: "spl-signer-wsol-token",
-      out: "signer-pico-token",
+      out: "signer-picosol-token",
     });
   });
 
   it("spl-stake-pool-withdraw-sol", async () => {
     await withdrawSolFixturesTest(1000000n, PICOSOL_MINT, {
-      inp: "signer-pico-token",
+      inp: "signer-picosol-token",
       out: "spl-signer-wsol-token",
     });
   });
@@ -24,7 +25,15 @@ describe("SPL Test", async () => {
   it("spl-stake-pool-deposit-stake", async () => {
     await depositStakeFixturesTest(PICOSOL_MINT, {
       inp: "picosol-deposit-stake",
-      out: "signer-pico-token",
+      out: "signer-picosol-token",
     });
+  });
+
+  it("spl-stake-pool-prefund-withdraw-stake", async () => {
+    await prefundWithdrawStakeFixturesTest(
+      1_000_000_000n,
+      PICOSOL_MINT,
+      "signer-picosol-token"
+    );
   });
 });
